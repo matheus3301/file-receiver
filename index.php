7faf41f2929b0979f2463985fc1f779042c0f112
matheus3301 <monteiromatheus.rocha@gmail.com>
2019-09-02 12:04:17 -0300

    Upload de arquivo único funcionando

--- index.php
+++ index.php
@@ -5,18 +5,147 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Enviar Arquivos</title>
-  <link rel="stylesheet" href="css/basic.css">
+  
   <link rel="stylesheet" href="css/style.css">
 
 
 </head>
 <body>
-  <h2>Enviar Arquivos</h2>
+    <style>
+        .uploaded{
+            margin-top:20px;
+        }
+
+        .file{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-bottom:20px;
+        }
+
+        .fileData{
+            display: flex;
+            align-items: center;
+            justify-content: initial;
+            
+        }
+
+        .fileData img{
+
+            width: 30px;
+        }
+
+        .fileStatus img{
+            width: 30px;
+        }
+
+        .btnUpload{
+            color:#fff;
+            background-color: rgb(64, 223, 64);
+            text-align: center;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            width: 100%;
+            border:0;
+            border-radius: 4px;
+            
+            cursor:pointer;
+            
+
+        }
+
+        .btnUpload:disabled{
+            color:#fff;
+            background-color: rgba(64, 223, 64,0.6);
+            text-align: center;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            width: 100%;
+            border:0;
+            border-radius: 4px;
+
+            cursor: not-allowed;
+            
+
+        }
+    </style>
+  
   <div class="container">
-  <form name="fileUploader" action="valida/upload.php" class="dropzone"></form>
-   
+        <center><h2>Enviar Arquivos</h2></center><br>
+  <form name="fileUploader" action="valida/upload.php" class="dropzone"><input onChange="addFile(this.files)"type="file" multiple="" class="input" >
+        <div class="message">
+            <span>Solte os arquivos aqui ;-;</span>
+        </div>
+  </form>
+  <div class="uploaded">
+      
+      
+    
+  </div>
+  <button class="btnUpload" id="btn"onClick="uploadFiles()" disabled>Enviar</button>
+        
    </body>
   </div>
-<script src="js/dropzone.js"></script>  
+  <script src="js/jquery.min.js"></script>
+  <script>
+    var toUpload = [];
+
+    function addFile(files){
+        console.log(files);
+        document.getElementById("btn").disabled = false;
+
+        for(var i = 0; i < files.length;i++){
+            toUpload += files[i];    
+
+            $(".uploaded").html($(".uploaded").html()+'<div class="file"><div class="fileData"><img src="assets/mimes/'+files[i].type+'.png"  alt=""><span class="fileName">'+files[i].name+'</span></div><div class="fileStatus"><img id="img'+i+'"src="assets/loading.png" alt="Status" ></div></div>');
+        
+            uploadFiles(files[i],i);
+        }
+
+        
+
+    }
+
+    function uploadFiles(file,index){
+                        var that = this;
+                        var formData = new FormData();
+
+                        formData.append('file',file);
+
+                        $.ajax({
+                            async: true,
+                            data: formData,
+                            cache: false,
+                            contentType: false,
+                            processData: false,
+                            xhr: function () {
+                                var myXhr = $.ajaxSettings.xhr();
+                                if (myXhr.upload) {
+                                    //myXhr.upload.addEventListener('progress', that.progressHandling, false);
+                                }
+                                return myXhr;
+                            },
+                            url : "valida/upload.php",
+                            type : 'post',
+                            
+                            beforeSend : function(){
+                                //alert('Enviando');
+                                //$("#resultado").html("ENVIANDO...");
+                            }
+                            })
+                            .done(function(msg){
+                                $("#img"+index).attr("src","assets/checked.png");
+
+                                console.log(msg);
+                                
+                            })
+                            .fail(function(jqXHR, textStatus, msg){
+                                alert(msg);
+                            });      
+
+    }
+    
+    
+  </script>
 </body>
 </html>
--- valida/upload.php
+++ valida/upload.php
@@ -1,17 +1,16 @@
-<?php
-$ds          = DIRECTORY_SEPARATOR;  //1
- 
-$storeFolder = 'uploads';   //2
- 
-if (!empty($_FILES)) {
-     
-    $tempFile = $_FILES['file']['tmp_name'];          //3             
-      
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
-     
-    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
- 
-    move_uploaded_file($tempFile,$targetFile); //6
-     
-}
-?>     
+<?php 
+
+
+		$arquivo = $_FILES['file'];			
+
+        $src = "uploads/".$arquivo['name'];
+
+        if(move_uploaded_file($arquivo['tmp_name'], $src)){
+            echo "Sucesso";
+        }else{
+            echo "Erro";
+        }
+       
+
+
+?>
