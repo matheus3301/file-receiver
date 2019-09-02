8c00249dffc0e14a11625368aaa362babae8731b
matheus3301 <monteiromatheus.rocha@gmail.com>
2019-09-01 23:36:59 -0300

    Sem estilização do dropzone

--- index.php
+++ index.php
@@ -6,19 +6,16 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Enviar Arquivos</title>
   <link rel="stylesheet" href="css/basic.css">
-  <link rel="stylesheet" href="css/dropzone.css">
   <link rel="stylesheet" href="css/style.css">
 
 
 </head>
 <body>
-  <h2>Receber Arquivos</h2>
+  <h2>Enviar Arquivos</h2>
   <div class="container">
-      <form action="valida/validaUpload.php" class="dropzone">
-        <div class="fallback">
-          <input name="file" type="file" multiple />
-        </div>
-      </form>
+  <form name="fileUploader" action="valida/upload.php" class="dropzone"></form>
+   
+   </body>
   </div>
 <script src="js/dropzone.js"></script>  
 </body>
